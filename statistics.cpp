b27aeab9f314bea7d8b668cda044346da7d6e59b
bryantb1556 <bryantcurtisbailey@gmail.com>
2025-06-01 00:18:37 -0400

    Added class "deck" to store all cards in shoe

--- statistics.cpp
+++ statistics.cpp
@@ -1,5 +1,8 @@
-//Card counting simulator
-
+/*Card counting simulator
+Blackjack pays 3 to 2, insurance pays 2 to 1
+Dealer stands on hard 17+, dealer must hit on soft 17
+Hi-Lo card counting method
+*/
 #include<iostream>
 #include<iomanip>
 #include<vector>
@@ -11,12 +14,38 @@ using namespace std;
 const double cardProb = (1.0 / 13.0);//Probability of a single card being selected
 const int decksInShoe = 8;
 const int numCards = decksInShoe * 52;
+const int maxTens = 16 * decksInShoe;//10/J/Q/K; 4 suites, max number of "10 cards"
+const int maxAces = 4 * decksInShoe;//4 aces per deck,
 
-class Dealer
+class Deck
+{
+public:
+	void initalizeCards()
+	{
+		int num = 0;
+		for (int i = 0; i < numCards; i++)
+		{
+			cardsAvailable[i] = (num) % 13 + 1;
+			num++;
+		}
+	}
+private:
+	int cardsAvailable[numCards] = { 0 };
+};
+
+class Dealer//Encompasses all information regarding dealer cards
 {
 public:
 	Dealer() { cards.clear(); }//Constructor to initalize cards
 	~Dealer(){}//Destructor
+
+	int pullCard()
+	{
+		srand(time(NULL));//Initalizes seed
+		int index = rand() % numCards + 1;
+
+	}
+
 	void addCard(int card)
 	{
 		cards.push_back(card);
@@ -27,15 +56,20 @@ public:
 		cards.clear();
 	}
 
+	void houseStrategy(int houseTotal, int playerTotal)
+	{
+
+	}
+
 private:
 	vector<int> cards;
 };
 
-class Counter
+class Counter//Encompasses all data about player and active count 
 {
 public:
 	//Constructor initalizes count and vector holding cards
-	Counter() : count(0), split(false), numSplits(0)
+	Counter() : count(0), split(false), numSplits(0), money(0)
 	{
 		cards.clear();
 	}
@@ -55,29 +89,30 @@ public:
 		cards.clear();
 	}
 
+	void playerStrategy(int playerTotal, int houseTotal)
+	{
+
+	}
+
 private:
 	int count;//Holds current count
 	vector<int> cards;//Holds current cards
 	bool split;
 	int numSplits;
+	int money; 
 };
 
-class Game
+void Game()
 {
-public:
-	int pullCard()
-	{
-		srand(time(NULL));//Initalizes seed
-		int card = rand() % 13 + 1;//Generates random number between 1-13
-		return card;
-	}
-private:
+	Dealer house;
+	Counter player;
 
-};
+}
 
 int main()
 {
-	Game game;
+	Deck deck;
+	deck.initalizeCards();
 
 	return 0;
 }
