c241ddbb7d06ce6b5ae53e7c78d5aa2e8c972b33
bryantb1556 <bryantcurtisbailey@gmail.com>
2025-06-01 13:59:54 -0400

    Added additionally methods to classes to improved functionality

--- statistics.cpp
+++ statistics.cpp
@@ -12,38 +12,55 @@ Hi-Lo card counting method
 using namespace std;
 
 const double cardProb = (1.0 / 13.0);//Probability of a single card being selected
-const int decksInShoe = 8;
-const int numCards = decksInShoe * 52;
+const int decksInShoe = 8;//Number of decks
+const int cardsInDeck = 52;//Number of cards
 const int maxTens = 16 * decksInShoe;//10/J/Q/K; 4 suites, max number of "10 cards"
 const int maxAces = 4 * decksInShoe;//4 aces per deck,
 
 class Deck
 {
 public:
+	Deck() { cardsAvailable.clear(); }//Constructor
+	~Deck() {};//Destructor
+
 	void initalizeCards()
 	{
 		int num = 0;
-		for (int i = 0; i < numCards; i++)
+		for (int i = 0; i < (decksInShoe * cardsInDeck); i++)
 		{
-			cardsAvailable[i] = (num) % 13 + 1;
+			cardsAvailable.push_back((num) % 13 + 1);
 			num++;
 		}
 	}
+
+	int getCard(int index)
+	{
+		int card = cardsAvailable[index];
+		cardsAvailable.erase(cardsAvailable.begin() + index);
+		return card;
+	}
+
 private:
-	int cardsAvailable[numCards] = { 0 };
+	vector<int> cardsAvailable;
 };
 
 class Dealer//Encompasses all information regarding dealer cards
 {
 public:
-	Dealer() { cards.clear(); }//Constructor to initalize cards
+	Dealer() { cards.clear(); deck.initalizeCards(); srand(time(NULL)); }//Constructor to initalize cards
 	~Dealer(){}//Destructor
+
+	void startDeck()
+	{
+		deck.initalizeCards();
+	}
 
 	int pullCard()
 	{
-		srand(time(NULL));//Initalizes seed
-		int index = rand() % numCards + 1;
-
+		int index = rand() % numCards;//Accesses vector location between 0 - numCards
+		int pulledCard = deck.getCard(index);
+		numCards--; 
+		return pulledCard;
 	}
 
 	void addCard(int card)
@@ -63,6 +80,8 @@ public:
 
 private:
 	vector<int> cards;
+	Deck deck;
+	int numCards = (decksInShoe * 52);
 };
 
 class Counter//Encompasses all data about player and active count 
@@ -111,8 +130,12 @@ void Game()
 
 int main()
 {
-	Deck deck;
-	deck.initalizeCards();
+	Dealer house;
+	for (int i = 0; i < 100; i++)
+	{
+		int x = house.pullCard();
+		cout << x << " ";
+	} 
 
 	return 0;
 }
